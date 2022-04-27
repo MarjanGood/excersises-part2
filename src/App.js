56f8889b369bf0215b863e3e91cfbe436a55faae
marjangoodarzi@gmail.com <marjangoodarzi@gmail.com>
2022-04-27 16:01:30 +0430

    session54

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+constructor(props){
+  super(props)
+
+  // this.state ={
+  //   meaningOfLife:47 + this.props.increment
+  // }
+
+}
+
+
+state ={
+  meaningOfLife:47 
+}
+
+handleClick=() =>{
+  this.setState((prevState, prevProps)=>{
+
+    return {meaningOfLife: prevState.meaningOfLife + prevProps.increment}
+   // return {meaningOfLife: prevState.meaningOfLife + this.props.increment}
+   // return {meaningOfLife: this.state.meaningOfLife + this.props.increment}
+
+  },
+  ()=> console.log(this.state.meaningOfLife))
+}
+
+render(){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {this.state.meaningOfLife}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={this.handleClick}>
+         Update state
+        </button>
       </header>
     </div>
   );
+}
 }
 
 export default App;
