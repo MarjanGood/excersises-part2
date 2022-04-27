786c397c6cde27490c31533ceaf7225109d7d9d9
marjangoodarzi@gmail.com <marjangoodarzi@gmail.com>
2022-04-27 17:23:17 +0430

    session54

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App increment={1} />
+    <App />
+
+    {/* <App increment={1} /> */}
   </React.StrictMode>
 );
 
